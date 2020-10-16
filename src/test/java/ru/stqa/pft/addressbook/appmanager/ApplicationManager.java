deaac28625ded70b02417e7b7cdbc259f6dd9121
burinskiypavel <54236839+burinskiypavel@users.noreply.github.com>
2020-10-16 12:09:59 +0300

    Initial commit

---
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,39 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    public WebDriver driver;
+
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+
+    public void init() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.get("http://localhost:8080/addressbook/group.php");
+        groupHelper = new GroupHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
+        sessionHelper = new SessionHelper(driver);
+        sessionHelper.login("admin", "secret");
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
+    }
+
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
+    }
+}
