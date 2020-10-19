4b1e37a8d869accb650fdf5b1b98b96eb6e96c57
burinskiypavel <54236839+burinskiypavel@users.noreply.github.com>
2020-10-19 14:47:21 +0300

    add checking for type

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,22 +3,39 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     public WebDriver driver;
 
+
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        driver = new ChromeDriver(chromeOptions);
+        if(browser.equals(BrowserType.CHROME)){
+            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            driver = new ChromeDriver(chromeOptions);
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "driver/geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            System.setProperty("webdriver.ie.driver", "driver/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+        }
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.get("http://localhost:8080/addressbook/group.php");
+        driver.navigate().to("http://localhost:8080/addressbook/group.php");
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
